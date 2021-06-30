c1bc6ebf8ba35f447f02f5200f777408d2f7a655
karmak40 <karmak40@outlook.com>
2021-06-30 15:56:04 +0200

    Resize add

--- src/app/canvas-signature/canvas-signature.component.ts
+++ src/app/canvas-signature/canvas-signature.component.ts
@@ -7,34 +7,34 @@ import { SaveService } from '../services/SaveService';
   styleUrls: ['./canvas-signature.component.css']
 })
 export class CanvasSignatureComponent implements OnInit {
- 
-  @Input() name: string | undefined;
-  @ViewChild('sigPad', {static: false}) sigPad: ElementRef | undefined;
+
+  @ViewChild('sigPad', { static: false }) sigPad: ElementRef | undefined;
 
   sigPadElement: any;
-  public context: any;;
+  public context: any;
   isDrawing = false;
   img: any;
 
-  lineWidth = 5;
+  //@Input() width: number = 300;
+  //@Input() height: number = 150;
+  @Input() lineWidth: number = 5;
+  @Input() fillStyle: string = '#333';
+  @Input() strokeStyle: string = '#333';
+  @Input() shadowColor: string = '#333';
+
   halfLineWidth = this.lineWidth / 2;
-  fillStyle = '#333';
-  strokeStyle = '#333';
-  shadowColor = '#333';
   shadowBlur = this.lineWidth / 4;
 
   constructor(private saveService: SaveService) { }
 
-  ngOnInit() {
-    // do not load here because at this moment view is not loaded yet
-  }
+  ngOnInit() { }
 
   ngAfterViewInit() {
     console.log('sigPad on after view init', this.sigPad);
     this.sigPadElement = this.sigPad.nativeElement;
     this.context = this.sigPadElement.getContext('2d');
     this.context.strokeStyle = '#3742fa';
-}
+  }
 
   @HostListener('document:mouseup', ['$event'])
   public onMouseUp(e: any) {
@@ -46,32 +46,31 @@ export class CanvasSignatureComponent implements OnInit {
     this.isDrawing = false;
   }
 
-
   public handleWritingStart(event) {
     event.preventDefault();
-  
+
     const mousePos = this.getMosuePositionOnCanvas(event);
-    
+
     this.context.beginPath();
-  
+
     this.context.moveTo(mousePos.x, mousePos.y);
-  
+
     this.context.lineWidth = this.lineWidth;
     this.context.strokeStyle = this.strokeStyle;
     this.context.shadowColor = null;
     this.context.shadowBlur = null;
-  
+
     this.context.fill();
-    
+
     this.isDrawing = true;
   }
 
   public handleWritingInProgress(event) {
     event.preventDefault();
-    
+
     if (this.isDrawing) {
       const mousePos = this.getMosuePositionOnCanvas(event);
-  
+
       this.context.lineTo(mousePos.x, mousePos.y);
       this.context.stroke();
     }
@@ -79,34 +78,34 @@ export class CanvasSignatureComponent implements OnInit {
 
   public handleDrawingEnd(event) {
     event.preventDefault();
-    
+
     if (this.isDrawing) {
       this.context.shadowColor = this.shadowColor;
       this.context.shadowBlur = this.shadowBlur;
-  
+
       this.context.stroke();
     }
-    
+
     this.isDrawing = false;
   }
 
   public handleClearButtonClick(event) {
     event.preventDefault();
-    
+
     this.clearCanvas();
   }
 
   public clearCanvas() {
-    this.context.clearRect(0, 0,  this.sigPadElement.width,  this.sigPadElement.height);
+    this.context.clearRect(0, 0, this.sigPadElement.width, this.sigPadElement.height);
   }
 
-  getMosuePositionOnCanvas (event) {
+  getMosuePositionOnCanvas(event) {
     var rect = this.sigPadElement.getBoundingClientRect();
     return {
-        x: ( (event.clientX || event.touches[0].clientX) - rect.left) / (rect.right - rect.left) * this.sigPadElement.width,
-        y: ( (event.clientY || event.touches[0].clientY) - rect.top) / (rect.bottom - rect.top) * this.sigPadElement.height
+      x: ((event.clientX || event.touches[0].clientX) - rect.left) / (rect.right - rect.left) * this.sigPadElement.width,
+      y: ((event.clientY || event.touches[0].clientY) - rect.top) / (rect.bottom - rect.top) * this.sigPadElement.height
     };
-}
+  }
 
   public clear() {
     this.context.clearRect(0, 0, this.sigPadElement.width, this.sigPadElement.height);
@@ -115,9 +114,7 @@ export class CanvasSignatureComponent implements OnInit {
 
   public save() {
     this.img = this.sigPadElement.toDataURL("image/png");
-
     this.saveService.saveImageData(this.img).subscribe()
-
     console.log(this.img);
   }
 }
